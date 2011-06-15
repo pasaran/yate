15ccb6939379eb4b7a6e0c0d5bff2235822b3fc7
nop <nop@yandex-team.ru>
2011-06-15 10:36:31 +0400

    Bright colors

--- colors.js
+++ colors.js
@@ -4,27 +4,39 @@
 
 var colors = {
 
+    // Styles
     'bold'      : [1,  22],
     'italic'    : [3,  23],
     'underline' : [4,  24],
     'inverse'   : [7,  27],
-    'red'       : [31, 39],
+
+    // Dark colors
+    'gray'      : [30, 39],
+    'maroon'    : [31, 39],
     'green'     : [32, 39],
-    'yellow'    : [33, 39],
-    'blue'      : [34, 39],
-    'magenta'   : [35, 39],
-    'cyan'      : [36, 39],
-    'white'     : [37, 39],
-    'grey'      : [90, 39],
-    'black'     : [90, 39]
+    'olive'     : [33, 39],
+    'navy'      : [34, 39],
+    'purple'    : [35, 39],
+    'teal'      : [36, 39],
+    'silver'    : [37, 39],
+
+    // Bright colors
+    'black'     : [90, 39],
+    'red'       : [91, 39],
+    'lime'      : [92, 39],
+    'yellow'    : [93, 39],
+    'blue'      : [94, 39],
+    'fuchsia'   : [95, 39],
+    'aqua'      : [96, 39],
+    'white'     : [97, 39]
 
 };
 
 for (var color in colors) {
     String.prototype.__defineGetter__(color, (function () {
-        var _color = color;
+        var _color = colors[color];
         return function() {
-            return '\033[' + colors[_color][0] + 'm' + this + '\033[' + colors[_color][1] + 'm';
+            return '\033[' + _color[0] + 'm' + this + '\033[' + _color[1] + 'm';
         }
     })());
 }
