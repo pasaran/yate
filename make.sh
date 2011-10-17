7a4d3de34a78f473206f16c6ad973fca103b3c1d
Sergei Nikitin <nik.pasaran@gmail.com>
2011-10-17 13:43:56 +0400

    lib -> src

--- make.sh
+++ make.sh
@@ -2,9 +2,9 @@
 
 TARGET="yate.js"
 
-cat lib/common.js lib/types.js lib/ast.js lib/parser.js lib/grammar.js lib/codetemplates.js > $TARGET
-cat lib/ast/*.js >> $TARGET
-cat lib/ast/yate/*.js >> $TARGET
-cat lib/ast/js/*.js >> $TARGET
-cat lib/yate.js >> $TARGET
+cat src/common.js src/types.js src/ast.js src/parser.js src/grammar.js src/codetemplates.js > $TARGET
+cat src/ast/*.js >> $TARGET
+cat src/ast/yate/*.js >> $TARGET
+cat src/ast/js/*.js >> $TARGET
+cat src/yate.js >> $TARGET
 
