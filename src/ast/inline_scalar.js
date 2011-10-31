2c2c456f2d7d0d0f3d7debe159d3060b9adaf3d3
Sergei Nikitin <nik.pasaran@gmail.com>
2011-10-31 16:02:49 +0400

    inline_scalar не items

--- src/ast/inline_scalar.js
+++ src/ast/inline_scalar.js
@@ -1,11 +1,14 @@
 yate.AST.inline_scalar = {};
 
 yate.AST.inline_scalar.options = {
-    base: 'inline_expr',
-    mixin: 'items'
+    base: 'inline_expr'
 };
 
 yate.AST.inline_scalar.oncast = function(to) {
     this.Expr.cast(to);
+};
+
+yate.AST.inline_scalar._getType = function() {
+    return this.Expr.type();
 };
 
