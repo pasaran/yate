99d5b9c70b486196c91c3b5b9450b0ea87923940
Sergei Nikitin <nik.pasaran@gmail.com>
2011-10-31 13:55:33 +0400

    object literal -> отдельные функции

--- src/ast/inline_function.js
+++ src/ast/inline_function.js
@@ -1,44 +1,42 @@
-yate.AST.inline_function = {
+yate.AST.inline_function = {};
 
-    options: {
-        base: 'inline_expr'
-    },
+yate.AST.inline_function.options = {
+    base: 'inline_expr'
+},
 
-    _getType: function() {
-        return this.def.type();
-    },
+yate.AST.inline_function._getType = function() {
+    return this.def.type();
+},
 
-    action: function() {
-        var def = this.def = this.scope.findFunction(this.Name);
-        if (!this.def) {
-            this.error('Undefined function ' + this.Name);
+yate.AST.inline_function.action = function() {
+    var def = this.def = this.scope.findFunction(this.Name);
+    if (!this.def) {
+        this.error('Undefined function ' + this.Name);
+    }
+
+    if (def.Type == yate.AST.function_type.USER) {
+        this.Fid = def.Fid;
+    } else if (def.Type == yate.AST.function_type.KEY) {
+        this.Kid = def.Kid;
+    }
+};
+
+yate.AST.inline_function.prepare = function() {
+    var def = this.def;
+    var args = this.Args.Items;
+
+    if (def.Type == yate.AST.function_type.KEY) {
+        args[0].cast(yate.types.SCALAR);
+    } else if (def.Type == yate.AST.function_type.INTERNAL) {
+        var argTypes = def._argTypes;
+        for (var i = 0, l = args.length; i < l; i++) {
+            args[0].cast(argTypes[i] || yate.types.SCALAR);
         }
-
-        if (def.Type == yate.AST.function_type.USER) {
-            this.Fid = def.Fid;
-        } else if (def.Type == yate.AST.function_type.KEY) {
-            this.Kid = def.Kid;
+    } else if (def.Type == yate.AST.function_type.USER) {
+        var defArgs = def.Args.Items;
+        for (var i = 0, l = args.length; i < l; i++) {
+            args[0].cast(defArgs[i].Typedef || yate.types.SCALAR);
         }
-    },
-
-    prepare: function() {
-        var def = this.def;
-        var args = this.Args.Items;
-
-        if (def.Type == yate.AST.function_type.KEY) {
-            args[0].cast(yate.types.SCALAR);
-        } else if (def.Type == yate.AST.function_type.INTERNAL) {
-            var argTypes = def._argTypes;
-            for (var i = 0, l = args.length; i < l; i++) {
-                args[0].cast(argTypes[i] || yate.types.SCALAR);
-            }
-        } else if (def.Type == yate.AST.function_type.USER) {
-            var defArgs = def.Args.Items;
-            for (var i = 0, l = args.length; i < l; i++) {
-                args[0].cast(defArgs[i].Typedef || yate.types.SCALAR);
-            }
-        }
-    },
-
+    }
 };
 
