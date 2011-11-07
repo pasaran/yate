2f81f8b46fc8cf92dd5cc04525a990cfb41334a0
Sergei Nikitin <nik.pasaran@gmail.com>
2011-11-07 16:23:28 +0400

    Секции в тестах

--- tests/tests.js
+++ tests/tests.js
@@ -1,63 +1,112 @@
 module.exports = [
+
     {
-        description: 'string interpolation',
-        yate: '01.yate',
-        data: '01.json',
-        result: 'Hello, nop'
+        title: 'string interpolation',
+        tests: [
+            {
+                description: 'string interpolation',
+                yate: 'string-interpolation.01.yate',
+                data: '01.json',
+                result: 'Hello, nop'
+            },
+            {
+                description: 'multiple string interpolations',
+                yate: 'string-interpolation.02.yate',
+                data: '01.json',
+                result: 'Hello, nop! Your ID is 33662468'
+            },
+            {
+                description: 'interpolation inside xml text',
+                yate: 'string-interpolation.03.yate',
+                data: '01.json',
+                result: '<h1>Hello, nop</h1>'
+            },
+            {
+                description: 'multiple interpolations inside xml text',
+                yate: 'string-interpolation.07.yate',
+                data: '01.json',
+                result: '<h1>Hello, nop. Your ID is 33662468</h1>'
+            },
+            {
+                description: 'interpolation inside xml',
+                yate: 'string-interpolation.04.yate',
+                data: '01.json',
+                result: '<h1>Hello, nop</h1>'
+            },
+            {
+                description: 'interpolation inside inline attribute',
+                yate: 'string-interpolation.05.yate',
+                data: '01.json',
+                result: '<h1 class="b-hello b-hello-33662468">Hello, nop</h1>'
+            },
+            {
+                description: 'multiple interpolations inside inline attribute',
+                yate: 'string-interpolation.06.yate',
+                data: '01.json',
+                result: '<h1 class="b-hello b-hello-nop b-hello-33662468">Hello, nop</h1>'
+            }
+        ]
     },
+
     {
-        description: 'xml text interpolation',
-        yate: '03.yate',
-        data: '01.json',
-        result: '<h1>Hello, nop</h1>'
+        title: 'xml attribute',
+        tests: [
+            {
+                description: 'inline attribute',
+                yate: 'attributes.01.yate',
+                data: '01.json',
+                result: '<h1 class="b-hello">Hello, nop</h1>'
+            },
+            {
+                description: 'xml attribute',
+                yate: 'attributes.02.yate',
+                data: '01.json',
+                result: '<h1 class="b-hello">Hello, nop</h1>'
+            },
+            {
+                description: 'xml attribute replaces inline attribute',
+                yate: 'attributes.03.yate',
+                data: '01.json',
+                result: '<h1 class="b-hello">Hello, nop</h1>'
+            },
+
+        ]
     },
+
     {
-        description: 'string interpolation inside xml',
-        yate: '02.yate',
-        data: '01.json',
-        result: '<h1>Hello, nop</h1>'
+        title: 'escaping',
+        tests: [
+            {
+                description: 'escape inline attribute',
+                yate: 'escaping.01.yate',
+                data: '01.json',
+                result: '<h1 class="b-hello &quot;Some &lt;b&gt;&amp;attribute&lt;/b&gt;&quot;">Hello, nop</h1>'
+            },
+            {
+                description: 'escape text',
+                yate: 'escaping.02.yate',
+                data: '01.json',
+                result: '<h1>&lt;b&gt;Some &amp;text&lt;/b&gt;</h1>'
+            },
+            {
+                description: 'escape xml text',
+                yate: 'escaping.03.yate',
+                data: '01.json',
+                result: '<h1>&lt;b&gt;Some &amp;text&lt;/b&gt;</h1>'
+            }
+        ]
     },
+
     {
-        description: 'string concatenation via +',
-        yate: '04.yate',
-        data: '01.json',
-        result: '<h1>Hello, nop</h1>'
-    },
-    {
-        description: 'inline attribute',
-        yate: '05.yate',
-        data: '01.json',
-        result: '<h1 class="b-hello">Hello, nop</h1>'
-    },
-    {
-        description: 'xml attribute',
-        yate: '06.yate',
-        data: '01.json',
-        result: '<h1 class="b-hello">Hello, nop</h1>'
-    },
-    {
-        description: 'xml attribute replaces inline attribute',
-        yate: '07.yate',
-        data: '01.json',
-        result: '<h1 class="b-hello">Hello, nop</h1>'
-    },
-    {
-        description: 'string interpolation in inline attribute',
-        yate: '08.yate',
-        data: '01.json',
-        result: '<h1 class="b-hello b-hello-33662468">Hello, nop</h1>'
-    },
-    {
-        description: 'quoted interpolation inside inline attribute',
-        yate: '09.yate',
-        data: '01.json',
-        result: '<h1 class="b-hello &quot;Some &lt;b&gt;&amp;attribute&lt;/b&gt;&quot;">Hello, nop</h1>'
-    },
-    {
-        description: 'quoted xml text',
-        yate: '10.yate',
-        data: '01.json',
-        result: '<h1>&lt;b&gt;Some &amp;text&lt;/b&gt;</h1>'
-    },
+        title: 'misc',
+        tests: [
+            {
+                description: 'string concatenation via +',
+                yate: 'misc.01.yate',
+                data: '01.json',
+                result: '<h1>Hello, nop</h1>'
+            }
+        ]
+    }
 ];
 
