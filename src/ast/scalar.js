6553f53eadc4d4a9f1cb57cba13493e7e31741f9
Sergei Nikitin <nik.pasaran@gmail.com>
2011-11-10 15:31:53 +0400

    Кастим scalar в oncast, а не в prepare

--- src/ast/scalar.js
+++ src/ast/scalar.js
@@ -1,18 +1,22 @@
 yate.AST.scalar = {};
 
-// yate.AST.scalar._type = yate.types.SCALAR;
-
 yate.AST.scalar._getType = function() {
     return this.Block.type();
 };
 
 yate.AST.scalar.prepare = function() {
-    //// this.Block.cast(yate.types.SCALAR);
-    this.Block.cast();
     /*
     if (this.AsListItem) {
         this.Block.rid();
     }
     */
+};
+
+yate.AST.scalar.oncast = function(to) {
+    this.Block.cast(to);
+};
+
+yate.AST.scalar.closes = function() {
+    return ( this.type() != yate.types.ATTR ); // Если тип атрибут, то после него все еще могут быть другие атрибуты.
 };
 
