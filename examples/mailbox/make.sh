183e9e5473eb7fcbe7f75a392055fdb01ff8395e
Sergei Nikitin <nik.pasaran@gmail.com>
2011-11-11 16:29:46 +0400

    fix

--- examples/mailbox/make.sh
+++ examples/mailbox/make.sh
@@ -5,6 +5,6 @@ cd ../..
 cd examples/mailbox
 ../../yate mailbox.yate > mailbox.js
 
-../../yate mailbox.yate mailbox.data.js > mailbox.js.html
+node ../run.js  mailbox.js mailbox.data.js > mailbox.js.html
 xsltproc mailbox.xsl mailbox.data.xml > mailbox.xsl.html
 
