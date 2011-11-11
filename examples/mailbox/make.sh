03fb73c17b5ff5c1c52ce9a0dbe46480a6536c95
Sergei Nikitin <nik.pasaran@gmail.com>
2011-11-11 16:17:55 +0400

    Поправлена сборка

--- examples/mailbox/make.sh
+++ examples/mailbox/make.sh
@@ -3,8 +3,8 @@
 cd ../..
 ./make.sh
 cd examples/mailbox
-node ../../yate.js mailbox.yate > mailbox.js
+../../yate mailbox.yate > mailbox.js
 
-node ../run.js mailbox.js mailbox.data.js > mailbox.js.html
+../../yate mailbox.yate mailbox.data.js > mailbox.js.html
 xsltproc mailbox.xsl mailbox.data.xml > mailbox.xsl.html
 
