7963ac33fb81fee85dec4ddca7c29830c0bf34d6
Sergei Nikitin <nik.pasaran@gmail.com>
2011-11-18 16:23:04 +0400

    position() -> index()

--- src/ast/inline_internal_function.js
+++ src/ast/inline_internal_function.js
@@ -12,7 +12,7 @@ yate.AST.internalFunctions = {
     'true': yate.AST.make('inline_internal_function', 'true', 'boolean'),
     'false': yate.AST.make('inline_internal_function', 'false', 'boolean'),
     'name': yate.AST.make('inline_internal_function', 'name', 'scalar'),
-    'position': yate.AST.make('inline_internal_function', 'position', 'scalar'),
+    'index': yate.AST.make('inline_internal_function', 'index', 'scalar'),
     'count': yate.AST.make('inline_internal_function', 'count', 'scalar'),
     'slice': yate.AST.make('inline_internal_function', 'slice', 'scalar', [ 'scalar', 'scalar', 'scalar' ])
 };
