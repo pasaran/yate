71193d3312aa1dda6a826b5b1ccc6b237007c37d
Sergei Nikitin <nik.pasaran@gmail.com>
2012-07-19 15:14:30 +0400

    Чиню тесты

--- tests/functions.09.js
+++ tests/functions.09.js
@@ -1,12 +1,8 @@
-Yater.externals = {
-
-    reverse: function(c0, a0, index, count, nodeset) {
-        var r = [];
-        for (var i = nodeset.length; i--;) {
-            r.push( nodeset[i] );
-        }
-        return r;
+yr.externals.reverse = function(c0, a0, index, count, nodeset) {
+    var r = [];
+    for (var i = nodeset.length; i--;) {
+        r.push( nodeset[i] );
     }
-
+    return r;
 };
 
